82241513ad6f4d1316a9af585ebc7d04aea5892e
Shamel Lakin <christlife4u@gmail.com>
2020-06-01 13:11:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,6 +6,9 @@ class Student
     @name = name 
     @grade = grade
   end 
+  
+  def self.create_table
+  end 
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
