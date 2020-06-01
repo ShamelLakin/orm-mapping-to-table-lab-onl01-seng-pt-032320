120b20fadc54475e95a068d653240e0564f92839
Shamel Lakin <christlife4u@gmail.com>
2020-06-01 13:25:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,9 +26,7 @@ class Student
   def save 
   end 
   
-  
-
-  # Remember, you can access your database connection anywhere in this class
+   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
 end
